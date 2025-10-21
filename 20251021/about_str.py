d8496458716d76f5229796cd10088d3df56b118c
none <gennady.konenko@gmail.com>
2025-10-21 20:15:27 +0300

    Введение в строку

---
+++ 20251021/about_str.py
@@ -0,0 +1,20 @@
+# print ('Tom\'s ball')
+# print ("I said \"yes\"")
+# print ('Каждый\nПитон\nЖелает') # Перенос строки \n
+# raduga = '''Каждый
+# Охотник
+# Желает'''
+# print (type(raduga))
+start = 'bad'
+finish = 'minton'
+word = start + finish
+# word = 125
+#print (word)
+#height = 155
+#shoes = 10
+#print ('Сумма моего роста и кросовок', )
+#print (ord('М'))
+#print (ord('м'))
+# \t - Табуляция
+# \x - 16 
+print('\x6d\x61\x6d\x61 \x6d\x79\x6c\x61 \x72\x61\x6d\x75')
