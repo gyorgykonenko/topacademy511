71439fb2684228b1b3df2d7098fd08bfa14ed9ca
none <gennady.konenko@gmail.com>
2025-11-11 21:01:50 +0300

    Добавлен код

--- 20251111/ex4_list_tuple.py
+++ 20251111/ex4_list_tuple.py
@@ -7,7 +7,7 @@ print("ids:", ids)
 
 ids.append(5)  # добавляем еще новый элемент списка
 ids.append(3)
-ids.append("Q")
+ids.append("35")
 print("ids:", ids)
 
 print("Длина списка ids:", len(ids))
