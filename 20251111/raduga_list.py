8df92bc576bd7c5411b877760ffb4cd24d970381
none <gennady.konenko@gmail.com>
2025-11-11 22:03:00 +0300

    Добавлено в задаче отображение индекса

--- 20251111/raduga_list.py
+++ 20251111/raduga_list.py
@@ -2,12 +2,12 @@
 raduga = [] # пустой список
 
 for i in range(7):
-    add_color = input("Введите цвет радуги, вариант %i: " % (i + 1))
+    add_color = input("Введите %i цвет радуги: " % (i + 1))
     raduga.append(add_color)
 
 print("Ваш список:", raduga)
 print("Еще раз ваш список")
 
 for add_color in raduga: # перебираем элементы списка
-    print(add_color)
-    
+    print(raduga.index(add_color), " - ", add_color)
+
