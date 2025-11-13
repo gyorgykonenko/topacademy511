260ef2de88715f72ac325235454748aec284a266
none <gennady.konenko@gmail.com>
2025-11-13 19:50:38 +0300

    Добавлены комментарии к коду задачи

--- 20251111/dz_phrases_konenko.py
+++ 20251111/dz_phrases_konenko.py
@@ -3,6 +3,7 @@
 # компьютер сначала запрашивает у пользователя 10 фраз
 # и помещает каждую фразу в список phrases;
 phrases = [] # пустой список
+# есть ещё изначально пустой список filtered_phrases (отфильтрованные фразы);
 filtered_phrases = [] # пустой фильтр
 kolvo = input('Введите количество фраз: ')
 kolvo = int(kolvo)
@@ -15,17 +16,11 @@ chislo = int(chislo)
 # далее компьютер выводит построчно фразы из списка phrases,
 for add_phrases in phrases:  # перебираем элементы списка
     if len(add_phrases) >= chislo:
+        # компьютер копирует из списка phrases в список filtered_phrases такие фразы
         filtered_phrases.append(add_phrases)
         print(add_phrases)
-        print(filtered_phrases)
-# но только те, чья длина больше, либо равна N.
-#    print(phrases.index(add_phrases), " - ", add_phrases)
-
-# for add_color in range(len(raduga)
-
-# Всё то же, что и на 10 баллов, но теперь:
-# есть ещё изначально пустой список filtered_phrases (отфильтрованные фразы);
-# компьютер копирует из списка phrases в список filtered_phrases такие фразы,
 # длины которых больше, либо равны N;
 # далее компьютер выводит построчно содержимое списка
 # filtered_phrases.
+        print(filtered_phrases)
+
