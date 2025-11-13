6f66283ec4b5fdbf9f6cc673f37d844cc1ed9e0a
gyorgykonenko <gennady.konenko@gmail.com>
2025-11-13 16:31:24 +0300

    Добавлены файлы-примеры кода с урока

---
+++ 20251111/dz_phrases_konenko.py
@@ -0,0 +1,29 @@
+# Есть изначально пустой список phrases.
+# Напишите программу, по которой:
+# компьютер сначала запрашивает у пользователя 10 фраз
+# и помещает каждую фразу в список phrases;
+phrases = [] # пустой список
+filtered_phrases = [] # пустой фильтр
+kolvo = input('Введите количество фраз: ')
+kolvo = int(kolvo)
+for i in range(kolvo):
+    add_phrases = input("Введите %i-ю фразу: " % (i + 1))
+    phrases.append(add_phrases)
+# потом компьютер запрашивает у пользователя целое число N;
+chislo = input('Введите целое число: ')
+chislo = int(chislo)
+# далее компьютер выводит построчно фразы из списка phrases,
+for add_phrases in phrases:  # перебираем элементы списка
+    if len(add_phrases) >= chislo:
+        print(add_phrases)
+# но только те, чья длина больше, либо равна N.
+#    print(phrases.index(add_phrases), " - ", add_phrases)
+
+# for add_color in range(len(raduga)
+
+# Всё то же, что и на 10 баллов, но теперь:
+# есть ещё изначально пустой список filtered_phrases (отфильтрованные фразы);
+# компьютер копирует из списка phrases в список filtered_phrases такие фразы,
+# длины которых больше, либо равны N;
+# далее компьютер выводит построчно содержимое списка
+# filtered_phrases.
---
+++ 20251111/str_len_range.py
@@ -0,0 +1,5 @@
+greeting = "Hello, world!"
+index = 0
+for index in range(len(greeting)):
+    print(f"Символ на индексе {index}: {greeting[index]}")
+    index += 1
